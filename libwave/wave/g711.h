8c0408810bf123bbf802a2b5826a188c2562c79a
Piotr J. Kacperski <piotrek321@gmail.com>
2022-03-27 11:52:33 +0200

    libwave: ephasize higher priority of "+" over "<<"
    without explicit parentheses around "+" operations used in conjunction with bit
    shifts clang 14 was generating noisy warnings

--- libwave/wave/g711.h
+++ libwave/wave/g711.h
@@ -16,11 +16,11 @@ constexpr std::int16_t decodeAlawByte(std::uint8_t logval)
   auto result = 0;
   if (exponent > 0)
   {
-    result = mantissa + 128 + 4 << exponent; // 128 = 16 << 3, 4 = 1/2 quantisation step
+    result = (mantissa + 128 + 4) << exponent; // 128 = 16 << 3, 4 = 1/2 quantisation step
   }
   else
   {
-    result = mantissa + 4 << 1; // 4 = 1/2 quantisation step
+    result = (mantissa + 4) << 1; // 4 = 1/2 quantisation step
   }
   return logval < 128 ? -result : result; // sign bit is inversed
 }
@@ -30,7 +30,7 @@ constexpr std::int16_t decodeUlawByte(std::uint8_t logval)
   logval ^= 0b1111'1111u;
   auto exponent = (logval & 0x70u) >> 4;
   auto mantissa = (logval & 0x0Fu) << 3; // 2 * mantissa * 2^2 (for 14 bit -> left shifted 16 bit conversion)
-  auto result = (33 << 2) + mantissa << exponent;
+  auto result = ((33 << 2) + mantissa) << exponent;
   return logval > 127 ? -result + (33 << 2) : result - (33 << 2);
 }
 
